4ed6c4f45c5d2360c634319159cc09f45c4160c3
mstreddy98 <109292497+mstreddy98@users.noreply.github.com>
2022-08-05 08:21:24 +0530

    Update index.js

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello, Welcome Khaja" }');
+    res.send('{ "response": "Hello, Welcome Swetha Reddy" }');
 });
 
 app.get('/will', function (req, res) {
